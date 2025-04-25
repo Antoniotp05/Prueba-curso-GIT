63b6282c8d352fc59b21c4e9d1464c20d3e54db2
Toño <antoniotp2019@gmail.com>
2025-04-25 13:46:01 +0200

    practica1

---
+++ Practica1.ino
@@ -0,0 +1,14 @@
+//Tiene el LED un tiempo de espera de un segundo que esta conectado a una placa arduino con una resistencia de 20 ohnios
+ 
+void setup()
+{
+pinMode (LED_BUITIN, OUTPUT);
+}
+
+void loop ()
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000); //Wait for 1000 millisecond(s)
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000); //Wait for 1000 millisecond(s)
+}
